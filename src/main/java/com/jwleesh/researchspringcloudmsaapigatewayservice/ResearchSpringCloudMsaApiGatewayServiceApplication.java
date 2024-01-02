40cd1d1f1fde620358c30896b0a89148dd267bc5
jwleesh <jwleesh@gmail.com>
2024-01-02 11:24:45 +0900

    source

---
+++ src/main/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jwleesh.researchspringcloudmsaapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchSpringCloudMsaApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchSpringCloudMsaApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwleesh.researchspringcloudmsaapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchSpringCloudMsaApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
