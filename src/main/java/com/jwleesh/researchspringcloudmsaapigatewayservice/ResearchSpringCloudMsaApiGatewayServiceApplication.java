d61747ece4aac3577dea125fcedcfec4b127e4a9
jwleesh <jwleesh@gmail.com>
2024-01-12 14:49:31 +0900

    source

--- src/main/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplication.java
+++ src/main/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.jwleesh.researchspringcloudmsaapigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResearchSpringCloudMsaApiGatewayServiceApplication {
@@ -10,4 +13,8 @@ public class ResearchSpringCloudMsaApiGatewayServiceApplication {
         SpringApplication.run(ResearchSpringCloudMsaApiGatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
