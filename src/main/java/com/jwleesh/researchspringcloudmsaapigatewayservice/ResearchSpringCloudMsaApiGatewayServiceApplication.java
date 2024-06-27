0ecc099d9a64dd00528acdc5f111eded70763ef3
jwlee <jwleesh@gmail.com>
2024-06-28 04:12:24 +0900

    @EnableEurekaClient 추가함
    동적 라우팅 (lb://SERVICE-NAME):
    
    @EnableEurekaClient가 있어야 Eureka 서버와 통신하여 서비스 인스턴스를 동적으로 찾을 수 있습니다.
    이 경우, @EnableEurekaClient는 애플리케이션을 Eureka 클라이언트로 등록하여 Eureka 서버에서 다른 서비스들의 위치를 동적으로 검색할 수 있게 합니다.

--- src/main/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplication.java
+++ src/main/java/com/jwleesh/researchspringcloudmsaapigatewayservice/ResearchSpringCloudMsaApiGatewayServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ResearchSpringCloudMsaApiGatewayServiceApplication {
 
     public static void main(String[] args) {
